154e676e666253ef2f497e7ca903b9b1aee5fec1
Adeniji Opeyemi <adenijiopeyemi68@gmail.com>
2022-02-03 12:53:54 +0100

    ...

--- src/routes/v1/routers/auth.router.ts
+++ src/routes/v1/routers/auth.router.ts
@@ -9,6 +9,8 @@ import { validateChannels as vcd } from '../../../middleware/header.mw'
 const router: Router = express.Router({ mergeParams: true });
 // import { protect, authorize } from '../../../middleware/auth.mw';
 
+const roles = ['superadmin', 'admin']
+
 router.post('/recruiters', vcd, addRecruiters);
 
 export default router;
